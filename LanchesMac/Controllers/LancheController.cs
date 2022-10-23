a5dca45f0b5f4df234916dff843a5d67697e7497
Moacir <moacir9401@gmail.com>
2022-10-23 10:44:15 -0300

    formatando tela lanches

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,24 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+
+            return View(lanches);
+        }
+    }
+}
